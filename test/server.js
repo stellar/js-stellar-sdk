29726bdcbee94666a4cb2862f496c60e10674b8f
Andrew Rogers <abennobashi@gmail.com>
2015-04-13 12:06:03 -0700

    Add CORS headers to test server

--- test/server.js
+++ test/server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 var app = express();
+
 app.use(bodyParser.json());
+app.use(allowCrossDomain);
 
 app.get('/', function(req, res) {
   res.status(200).send("hi");
