3b983be5b6d9bad404b7ce3abf521758373857b2
Andrew Rogers <abennobashi@gmail.com>
2015-04-28 14:31:30 -0700

    Add issued currency support in currency.js

--- src/currency.js
+++ src/currency.js
@@ -1,5 +1,5 @@
 import {Account} from "./account";
-import {xdr} from "stellar-base";
+import {xdr, Keypair} from "stellar-base";
 
 export class Currency {
 
@@ -7,34 +7,52 @@ export class Currency {
         return new Currency("XLM");
     }
 
-    static ISO4217(type, issuer) {
-        return new Currency(type, issuer);
+    static ISO4217(code, issuer) {
+        return new Currency(code, issuer);
     }
 
     /**
-    * A currency type describes a currency and issuer pair. In the case of the native
+    * A currency code describes a currency and issuer pair. In the case of the native
     * currency XLM, the issuer will be null.
     * @constructor
-    * @type {string} The currency code
+    * @param {string} code - The currency code.
+    * @param {string} issuer - The address of the issuer.
     */
-    constructor(type, issuer) {
-        this.type      = type;
-        this.issuer   = issuer ? issuer : null;
+    constructor(code, issuer) {
+        if (code.length != 3) {
+            throw new Error("Currency code must be 3 characters");
+        }
+        if (code !== "XLM" && !issuer) {
+            throw new Error("Issuer cannot be null");
+        }
+        this.code = code;
+        this.issuer = issuer;
     }
 
+    /**
+    * Returns the xdr object for this currency.
+    */
     toXdrObject() {
         if (this.isNative()) {
             return xdr.Currency.native();
         } else {
-            throw new Error("not implemented");
+            // need to pad the currency code with the null byte
+            var isoCurrencyIssuer = new xdr.IsoCurrencyIssuer({
+                currencyCode: this.code + "\0",
+                issuer: Keypair.fromAddress(this.issuer).publicKey()
+            });
+            var currency = xdr.Currency.iso4217();
+            currency.set("iso4217", isoCurrencyIssuer);
+
+            return currency;
         }
     }
 
     isNative() {
-        return this.type === "XLM";
+        return this.code === "XLM";
     }
 
     equals(currency) {
-        return this.type == currency.type && this.issuer == currency.issuer;
+        return this.code == currency.code && this.issuer == currency.issuer;
     }
 }
