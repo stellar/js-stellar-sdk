76d7ee87fcca1c82f108ef051eeb011fbe760a71
Andrew Rogers <abennobashi@gmail.com>
2015-05-01 10:50:42 -0700

    add fromXdr method to currency
    - also adds some documenation

--- src/currency.js
+++ src/currency.js
@@ -1,14 +1,31 @@
 import {Account} from "./account";
-import {xdr, Keypair} from "stellar-base";
+import {xdr, Keypair, encodeBase58Check} from "stellar-base";
 
+/**
+* Currency class represents a currency, either the native currency ("XLM")
+* or a currency code / issuer address pair.
+*/
 export class Currency {
 
+    /**
+    * Returns a currency object for the native currency.
+    */
     static native() {
         return new Currency("XLM");
     }
 
-    static ISO4217(code, issuer) {
-        return new Currency(code, issuer);
+    /**
+    * Returns a currency object from its XDR object representation.
+    * @param {xdr.Currency.iso4217} xdr - The currency xdr object.
+    */
+    static fromOperation(xdr) {
+        if (xdr._switch.name == "native") {
+            return this.native();
+        } else {
+            let code = xdr._value._attributes.currencyCode;
+            let issuer = encodeBase58Check("accountId", xdr._value._attributes.issuer);
+            return new this(code, issuer);
+        }
     }
 
     /**
@@ -19,13 +36,14 @@ export class Currency {
     * @param {string} issuer - The address of the issuer.
     */
     constructor(code, issuer) {
-        if (code.length != 3) {
-            throw new Error("Currency code must be 3 characters");
+        if (code.length != 3 && code.length != 4) {
+            throw new Error("Currency code must be 3 or 4 characters");
         }
         if (code !== "XLM" && !issuer) {
             throw new Error("Issuer cannot be null");
         }
-        this.code = code;
+        // pad code with null byte if necessary
+        this.code = code.length == 3 ? code + "\0" : code;
         this.issuer = issuer;
     }
 
@@ -38,7 +56,7 @@ export class Currency {
         } else {
             // need to pad the currency code with the null byte
             var isoCurrencyIssuer = new xdr.IsoCurrencyIssuer({
-                currencyCode: this.code + "\0",
+                currencyCode: this.code,
                 issuer: Keypair.fromAddress(this.issuer).publicKey()
             });
             var currency = xdr.Currency.iso4217();
@@ -48,10 +66,16 @@ export class Currency {
         }
     }
 
+    /**
+    * Returns true if this currency object is the native currency.
+    */
     isNative() {
-        return this.code === "XLM";
+        return this.code === "XLM\0";
     }
 
+    /**
+    * Returns true if this currency equals the given currency.
+    */
     equals(currency) {
         return this.code == currency.code && this.issuer == currency.issuer;
     }
