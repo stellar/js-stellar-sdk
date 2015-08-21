6e796d8938b95456e4a153214f85aef874b1757b
Bartek Nowotarski <bartek@nowotarski.info>
2015-08-21 16:22:31 +0200

    0.1.3

--- dist/stellar-lib.js
+++ dist/stellar-lib.js
@@ -2761,7 +2761,7 @@ var StellarLib =
 
 	            value: function submitTransaction(transaction) {
 	                var promise = axios.post(URI(this.serverURL).path("transactions").toString(), {
-	                    tx: transaction.toEnvelope().toXDR().toString("hex")
+	                    tx: transaction.toEnvelope().toXDR().toString("base64")
 	                }).then(function (response) {
 	                    return response.data;
 	                })["catch"](function (response) {
