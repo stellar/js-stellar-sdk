acbac12e25bdef0126283a865a642bbb3c365cc3
Bartek Nowotarski <bartek@nowotarski.info>
2015-08-25 16:43:40 +0200

    Removed dist and lib folders, publishing to npm and bower

---
+++ bower_publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone "https://stellar-jenkins@github.com/stellar/bower-js-stellar-lib.git" bower
+
+if [ ! -d "bower" ]; then
+  echo "Error cloning"
+  exit 1
+fi
+
+cp dist/* bower
+cd bower
+git add .
+git commit -m $TRAVIS_TAG
+git tag -a $TRAVIS_TAG -m $TRAVIS_TAG
+git push origin master --tags
+cd ..
+rm -rf bower
