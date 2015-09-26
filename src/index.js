f20069ded74aa7487fecdecfb03cdc6b7a0bb1d9
Bartek Nowotarski <bartek@nowotarski.info>
2015-09-26 17:45:08 +0200

    Expose everything from stellar-base

--- src/index.js
+++ src/index.js
@@ -2,13 +2,5 @@
 export * from "./errors";
 export {Server} from "./server";
 
-// expose classes from stellar-base
-export {Account} from "stellar-base";
-export {Transaction} from "stellar-base";
-export {TransactionBuilder} from "stellar-base";
-export {Asset} from "stellar-base";
-export {Operation} from "stellar-base";
-export {Keypair} from "stellar-base";
-export {Memo} from "stellar-base";
-export {Network, Networks} from "stellar-base";
-export {xdr} from "stellar-base";
+// expose classes and functions from stellar-base
+export * from "stellar-base";
