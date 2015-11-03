8a1de102ccd380b533dc45f9b36aef29551a594c
Mason Fischer <mason@thoughtbot.com>
2015-11-02 21:43:32 -0600

    Rename destination_type to destination_asset

--- src/path_call_builder.js
+++ src/path_call_builder.js
@@ -7,17 +7,17 @@ export class PathCallBuilder extends CallBuilder {
     /*
     * @constructor
     */
-    constructor(url, source, destination, destination_type, destination_amount) {
+    constructor(url, source, destination, destination_asset, destination_amount) {
         super(url);
         this.url.segment('paths');
         this.url.addQuery('destination_account', destination);
         this.url.addQuery('source_account', source);
         this.url.addQuery('destination_amount', destination_amount);
 
-        if (!destination_type.isNative()) {
+        if (!destination_asset.isNative()) {
             this.url.addQuery('destination_asset_type', 'credit_alphanum4');
-            this.url.addQuery('destination_asset_code', destination_type.getCode());
-            this.url.addQuery('destination_asset_issuer', destination_type.getIssuer());
+            this.url.addQuery('destination_asset_code', destination_asset.getCode());
+            this.url.addQuery('destination_asset_issuer', destination_asset.getIssuer());
         } else {
             this.url.addQuery('destination_asset_type', 'native');
         }
