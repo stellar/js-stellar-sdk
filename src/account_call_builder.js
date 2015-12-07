6d3f121d410ded6adb25b88f66e1874a5169cc26
Scott Fleckenstein <nullstyle@gmail.com>
2015-12-07 14:41:51 -0800

    Deprecate AccountCallBuilder#address in favor of accountID

--- src/account_call_builder.js
+++ src/account_call_builder.js
@@ -16,16 +16,25 @@ export class AccountCallBuilder extends CallBuilder {
     }
 
     /**
+     * @deprecated use accountID method instead
+     */
+    address(id) {
+        console.warn("AccountCallBuilder#address is deprecated, please use AccountCallBuilder#accountID instead");
+        return this.accountID(id);
+    }
+
+    /**
      * Returns information and links relating to a single account.
      * The balances section in the returned JSON will also list all the trust lines this account has set up.
      *
      * @see [Account Details](https://www.stellar.org/developers/horizon/reference/accounts-single.html)
-     * @param {string} accountId For example: `GDGQVOKHW4VEJRU2TETD6DBRKEO5ERCNF353LW5WBFW3JJWQ2BRQ6KDD`
+     * @param {id} accountId For example: `GDGQVOKHW4VEJRU2TETD6DBRKEO5ERCNF353LW5WBFW3JJWQ2BRQ6KDD`
      * @returns {AccountCallBuilder}
      */
-    address(accountId) {
-        this.filter.push(['accounts', accountId]);
-        return this;
+    accountID(id) {
+      this.filter.push(['accounts', id]);
+      return this;
     }
+
 
 }
