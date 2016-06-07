822e3a7eb5a96788545cafdca8ece56c3c6d4770
Bartek Nowotarski <bartek@nowotarski.info>
2016-06-07 16:34:00 +0200

    Changes to Server.loadAccount

--- test/unit/server_test.js
+++ test/unit/server_test.js
@@ -19,6 +19,112 @@ describe("server.js tests", function () {
     });
   });
 
+  describe('Server.loadAccount', function () {
+    let accountResponse = {
+      "_links": {
+        "self": {
+          "href": "https://horizon-testnet.stellar.org/accounts/GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS"
+        },
+        "transactions": {
+          "href": "https://horizon-testnet.stellar.org/accounts/GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS/transactions{?cursor,limit,order}",
+          "templated": true
+        },
+        "operations": {
+          "href": "https://horizon-testnet.stellar.org/accounts/GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS/operations{?cursor,limit,order}",
+          "templated": true
+        },
+        "payments": {
+          "href": "https://horizon-testnet.stellar.org/accounts/GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS/payments{?cursor,limit,order}",
+          "templated": true
+        },
+        "effects": {
+          "href": "https://horizon-testnet.stellar.org/accounts/GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS/effects{?cursor,limit,order}",
+          "templated": true
+        },
+        "offers": {
+          "href": "https://horizon-testnet.stellar.org/accounts/GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS/offers{?cursor,limit,order}",
+          "templated": true
+        }
+      },
+      "id": "GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS",
+      "paging_token": "5387216134082561",
+      "account_id": "GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS",
+      "sequence": "5387216134078475",
+      "subentry_count": 5,
+      "thresholds": {
+        "low_threshold": 0,
+        "med_threshold": 0,
+        "high_threshold": 0
+      },
+      "flags": {
+        "auth_required": false,
+        "auth_revocable": false
+      },
+      "balances": [
+        {
+          "balance": "0.0000000",
+          "limit": "922337203685.4775807",
+          "asset_type": "credit_alphanum4",
+          "asset_code": "AAA",
+          "asset_issuer": "GAX4CUJEOUA27MDHTLSQCFRGQPEXCC6GMO2P2TZCG7IEBZIEGPOD6HKF"
+        },
+        {
+          "balance": "5000.0000000",
+          "limit": "922337203685.4775807",
+          "asset_type": "credit_alphanum4",
+          "asset_code": "MDL",
+          "asset_issuer": "GAX4CUJEOUA27MDHTLSQCFRGQPEXCC6GMO2P2TZCG7IEBZIEGPOD6HKF"
+        },
+        {
+          "balance": "10000.0000000",
+          "limit": "922337203685.4775807",
+          "asset_type": "credit_alphanum4",
+          "asset_code": "USD",
+          "asset_issuer": "GAX4CUJEOUA27MDHTLSQCFRGQPEXCC6GMO2P2TZCG7IEBZIEGPOD6HKF"
+        },
+        {
+          "balance": "70.0998900",
+          "asset_type": "native"
+        }
+      ],
+      "signers": [
+        {
+          "public_key": "GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS",
+          "weight": 1
+        }
+      ],
+      "data": {}
+    };
+
+    it("returns AccountResponse object", function (done) {
+      this.axiosMock.expects('get')
+        .withArgs(sinon.match('https://horizon-live.stellar.org:1337/accounts/GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS'))
+        .returns(Promise.resolve({data: accountResponse}));
+
+      this.server.loadAccount("GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS")
+        .then(response => {
+          // Response data
+          expect(response.account_id).to.be.equal("GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS");
+          expect(response.subentry_count).to.be.equal(5);
+          expect(response.transactions).to.be.function;
+          expect(response.operations).to.be.function;
+          expect(response.payments).to.be.function;
+          expect(response.effects).to.be.function;
+          expect(response.offers).to.be.function;
+          // AccountResponse methods
+          expect(response.sequenceNumber()).to.be.equal("5387216134078475");
+          expect(response.sequence).to.be.equal("5387216134078475");
+          response.incrementSequenceNumber()
+          expect(response.sequenceNumber()).to.be.equal("5387216134078476");
+          expect(response.sequence).to.be.equal("5387216134078476");
+          done();
+        })
+        .catch(function (err) {
+          done(err);
+        });
+    })
+  });
+
   describe('Server._sendResourceRequest', function () {
 
     describe("requests all ledgers", function () {
