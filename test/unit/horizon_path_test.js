3d0c0441c14ab0c88eb53b0a7aa438cbd17a08a3
Fritz Ekwoge <fritz.ekwoge@feeperfect.com>
2017-11-06 14:59:40 +0100

    improved horizon path tests

--- test/unit/horizon_path_test.js
+++ test/unit/horizon_path_test.js
@@ -4,17 +4,6 @@ describe("horizon path tests", function () {
     this.axiosMock = sinon.mock(axios);
     StellarSdk.Config.setDefault();
     StellarSdk.Network.useTestNetwork();
-    let keypair = StellarSdk.Keypair.random();
-    let account = new StellarSdk.Account(keypair.publicKey(), "56199647068161");
-    this.transaction = new StellarSdk.TransactionBuilder(account)
-      .addOperation(StellarSdk.Operation.payment({
-        destination: "GASOCNHNNLYFNMDJYQ3XFMI7BYHIOCFW3GJEOWRPEGK2TDPGTG2E5EDW",
-        asset: StellarSdk.Asset.native(),
-        amount: "100.50"
-      }))
-      .build();
-    this.transaction.sign(keypair)
-
   });
 
   afterEach(function () {
@@ -22,220 +11,89 @@ describe("horizon path tests", function () {
     this.axiosMock.restore();
   });
 
-  describe('server url without folder path: https://acme.com:1337', function () {
-    let server = new StellarSdk.Server('https://acme.com:1337');
+  function test_horizon_paths(serverUrl) {
 
-    let callbuilder = null;
+    let server = new StellarSdk.Server(serverUrl);
 
+    let randomResult = {
+      data: {
+        url: serverUrl,
+        random: Math.round(1000 * Math.random()),
+        endpoint: "bogus"
+      }
+    };
 
+    function prepareAxios(axiosMock, endpoint) {
+      randomResult.endpoint = endpoint;
+      axiosMock.expects('get')
+        .withArgs(sinon.match(serverUrl + endpoint))
+        .returns(Promise.resolve(randomResult));
+    }
 
-    it("server.accounts()", function () {
-      callbuilder = server.accounts();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/accounts");
+    it("server.accounts() " + serverUrl, function (done) {
+      prepareAxios(this.axiosMock, "/accounts");
+      server.accounts().call().should.eventually.deep.equal(randomResult.data).notify(done);
     });
 
-
-    it('server.accounts().accountId("fooAccountId")', function () {
-      callbuilder = server.accounts().accountId("fooAccountId");
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/accounts/fooAccountId");
+    it("server.accounts().accountId('fooAccountId') " + serverUrl, function (done) {
+      prepareAxios(this.axiosMock, "/accounts/fooAccountId");
+      server.accounts().accountId('fooAccountId').call().should.eventually.deep.equal(randomResult.data).notify(done);
     });
 
-
-    it('server.transactions()', function () {
-      callbuilder = server.transactions();
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/transactions");
+    it("server.transactions() " + serverUrl, function (done) {
+      prepareAxios(this.axiosMock, "/transactions");
+      server.transactions().call().should.eventually.deep.equal(randomResult.data).notify(done);
     });
 
-
-    it('server.transactions().transaction("fooTransactionId")', function () {
-      callbuilder = server.transactions().transaction("fooTransactionId");
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/transactions/fooTransactionId");
-
+    it("server.transactions().transaction('fooTransactionId') " + serverUrl, function (done) {
+      prepareAxios(this.axiosMock, "/transactions/fooTransactionId");
+      server.transactions().transaction('fooTransactionId').call().should.eventually.deep.equal(randomResult.data).notify(done);
     });
 
-
-
-    it('server.transactions().transaction("fooTransactionId")', function () {
-      callbuilder = server.transactions().transaction("fooTransactionId");
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/transactions/fooTransactionId");
-
+    it("server.transactions().forAccount('fooAccountId') " + serverUrl, function (done) {
+      prepareAxios(this.axiosMock, "/accounts/fooAccountId/transactions");
+      server.transactions().forAccount('fooAccountId').call().should.eventually.deep.equal(randomResult.data).notify(done);
     });
 
+    it("server.submitTransaction() " + serverUrl, function (done) {
+      randomResult.endpoint = "post";
 
-
-    it('server.transactions().forAccount("fooAccountId")', function () {
-      callbuilder = server.transactions().forAccount("fooAccountId");
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/accounts/fooAccountId/transactions");
-
-    });
-
-    it('server.submitTransaction', function (done) {
+      let keypair = StellarSdk.Keypair.random();
+      let account = new StellarSdk.Account(keypair.publicKey(), "56199647068161");
+      
+      let fakeTransaction = new StellarSdk.TransactionBuilder(account)
+        .addOperation(StellarSdk.Operation.payment({
+          destination: keypair.publicKey(),
+          asset: StellarSdk.Asset.native(),
+          amount: "100.50"
+        }))
+        .build();
+      fakeTransaction.sign(keypair);
+      let tx = encodeURIComponent(fakeTransaction.toEnvelope().toXDR().toString("base64"));
 
       this.axiosMock.expects('post')
-        .withArgs(sinon.match('https://acme.com:1337/transactions', 'bogus'))
-        .returns(Promise.resolve({
-          ok: true
-        }));
+        .withArgs(sinon.match(serverUrl + "/transactions", `tx=${tx}`))
+        .returns(Promise.resolve(randomResult));
 
-      server.submitTransaction(this.transaction)
-        .then(response => {
-          done();
-        })
-        .catch(function (err) {
-          done(err);
-        });
-
+      server.submitTransaction(fakeTransaction).should.eventually.deep.equal(randomResult.data).notify(done);
     });
 
+  }
 
+  let serverUrls = [];
 
+  //server url without folder path.
+  serverUrls.push("https://acme.com:1337");
 
-  });
+  //server url folder path.
+  serverUrls.push("https://acme.com:1337/folder");
 
+  //server url folder and subfolder path.
+  serverUrls.push("https://acme.com:1337/folder/subfolder");
 
-  describe('server url with folder path: https://acme.com:1337/folder/', function () {
-    let server = new StellarSdk.Server('https://acme.com:1337/folder/');
-
-    let callbuilder = null;
-
-    it("server.accounts()", function () {
-      callbuilder = server.accounts();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/folder/accounts");
-    });
-
-
-    it('server.accounts().accountId("fooAccountId")', function () {
-      callbuilder = server.accounts().accountId("fooAccountId");
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/folder/accounts/fooAccountId");
-    });
-
-
-    it('server.transactions()', function () {
-      callbuilder = server.transactions();
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/folder/transactions");
-    });
-
-
-    it('server.transactions().transaction("fooTransactionId")', function () {
-      callbuilder = server.transactions().transaction("fooTransactionId");
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/folder/transactions/fooTransactionId");
-
-    });
-
-
-
-    it('server.transactions().transaction("fooTransactionId")', function () {
-      callbuilder = server.transactions().transaction("fooTransactionId");
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/folder/transactions/fooTransactionId");
-
-    });
-
-
-
-    it('server.transactions().forAccount("fooAccountId")', function () {
-      callbuilder = server.transactions().forAccount("fooAccountId");
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/folder/accounts/fooAccountId/transactions");
-
-    });
-
-    it('server.submitTransaction', function (done) {
-
-      this.axiosMock.expects('post')
-        .withArgs(sinon.match('https://acme.com:1337/folder/transactions', 'bogus'))
-        .returns(Promise.resolve({
-          ok: true
-        }));
-
-      server.submitTransaction(this.transaction)
-        .then(response => {
-          done();
-        })
-        .catch(function (err) {
-          done(err);
-        });
-
-    });
-
-
-  });
-
-  describe('server url with folder and subfolder path: https://acme.com:1337/folder/subfolder', function () {
-    let server = new StellarSdk.Server('https://acme.com:1337/folder/subfolder');
-
-    let callbuilder = null;
-
-    it("server.accounts()", function () {
-      callbuilder = server.accounts();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/folder/subfolder/accounts");
-    });
-
-
-    it('server.accounts().accountId("fooAccountId")', function () {
-      callbuilder = server.accounts().accountId("fooAccountId");
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/folder/subfolder/accounts/fooAccountId");
-    });
-
-
-    it('server.transactions()', function () {
-      callbuilder = server.transactions();
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/folder/subfolder/transactions");
-    });
-
-
-    it('server.transactions().transaction("fooTransactionId")', function () {
-      callbuilder = server.transactions().transaction("fooTransactionId");
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/folder/subfolder/transactions/fooTransactionId");
-
-    });
-
-
-
-    it('server.transactions().transaction("fooTransactionId")', function () {
-      callbuilder = server.transactions().transaction("fooTransactionId");
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/folder/subfolder/transactions/fooTransactionId");
-
-    });
-
-    it('server.transactions().forAccount("fooAccountId")', function () {
-      callbuilder = server.transactions().forAccount("fooAccountId");
-      callbuilder.checkFilter();
-      expect(callbuilder.url.toString()).to.be.equal("https://acme.com:1337/folder/subfolder/accounts/fooAccountId/transactions");
-
-    });
-
-    it('server.submitTransaction', function (done) {
-
-      this.axiosMock.expects('post')
-        .withArgs(sinon.match('https://acme.com:1337/folder/subfolder/transactions', 'bogus'))
-        .returns(Promise.resolve({
-          ok: true
-        }));
-
-      server.submitTransaction(this.transaction)
-        .then(response => {
-          done();
-        })
-        .catch(function (err) {
-          done(err);
-        });
-
-    });
-
-  });
-
+  for (var index = 0; index < serverUrls.length; index++) {
+    var serverUrl = serverUrls[index];
+    test_horizon_paths(serverUrl);
+  }
 
 });
