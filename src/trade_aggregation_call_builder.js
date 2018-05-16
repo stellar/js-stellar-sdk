cf918794c84dc2bdd52bca0b9922531e51771a8c
tomerweller <tomer.weller@gmail.com>
2018-05-16 12:09:35 -0700

    add one minute trade aggregation resolution (#164)

--- src/trade_aggregation_call_builder.js
+++ src/trade_aggregation_call_builder.js
@@ -1,7 +1,7 @@
 import { CallBuilder } from "./call_builder";
 import { NotFoundError, NetworkError, BadRequestError } from "./errors";
 
-const allowedResolutions = [300000, 900000, 3600000, 86400000, 604800000];
+const allowedResolutions = [60000, 300000, 900000, 3600000, 86400000, 604800000];
 
 /**
  * Trade Aggregations facilitate efficient gathering of historical trade data
@@ -11,7 +11,7 @@ const allowedResolutions = [300000, 900000, 3600000, 86400000, 604800000];
  * @param {Asset} counter counter asset
  * @param {long} start_time lower time boundary represented as millis since epoch
  * @param {long} end_time upper time boundary represented as millis since epoch
- * @param {long} resolution segment duration as millis since epoch. *Supported values are 5 minutes (300000), 15 minutes (900000), 1 hour (3600000), 1 day (86400000) and 1 week (604800000).
+ * @param {long} resolution segment duration as millis since epoch. *Supported values are 1 minute (60000), 5 minutes (300000), 15 minutes (900000), 1 hour (3600000), 1 day (86400000) and 1 week (604800000).
  * @returns {OrderbookCallBuilder}
  */
 export class TradeAggregationCallBuilder extends CallBuilder {
