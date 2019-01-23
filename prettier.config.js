78c0c817e8ff4fbcb663b5e019530d3254b01f3c
Morley Zhi <morley@lightyear.io>
2019-01-23 16:05:48 -0500

    add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  printWidth: 80,
+  proseWrap: "always",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+};
