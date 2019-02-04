968ebf41bbd7363401a8c8cb9b63112e76c3aa8a
Bartek Nowotarski <bartek@nowotarski.info>
2019-02-04 18:34:18 +0100

    Fix test timeout

--- test/integration/client_headers_test.js
+++ test/integration/client_headers_test.js
@@ -3,56 +3,63 @@ const url = require('url');
 const port = 3000;
 
 describe('integration tests', function() {
-  it("sends client via headers", function(done) {
-    if (typeof window !== "undefined") {
-      done();
-      return;
-    }
+  if (typeof window !== 'undefined') {
+    done();
+    return;
+  }
 
-    var server;
+  it('sends client via headers', function(done) {
+    let server;
 
     const requestHandler = (request, response) => {
       expect(request.headers['x-client-name']).to.be.equal('js-stellar-sdk');
-      expect(request.headers['x-client-version']).to.match(/^[0-9]+\.[0-9]+\.[0-9]+$/);
+      expect(request.headers['x-client-version']).to.match(
+        /^[0-9]+\.[0-9]+\.[0-9]+$/
+      );
       response.end();
       server.close(() => done());
-    }
+    };
 
     server = http.createServer(requestHandler);
     server.listen(port, (err) => {
       if (err) {
         done(err);
-        return
+        return;
       }
 
-      new StellarSdk.Server(`http://localhost:${port}`, {allowHttp: true}).operations().call();
-    })
+      new StellarSdk.Server(`http://localhost:${port}`, { allowHttp: true })
+        .operations()
+        .call();
+    });
   });
 
-  it("sends client data via get params when streaming", function(done) {
-    if (typeof window !== "undefined") {
-      done();
-      return;
-    }
-
-    var server;
+  it('sends client data via get params when streaming', function(done) {
+    let server;
+    let closeStream;
 
     const requestHandler = (request, response) => {
-      var query = url.parse(request.url, true).query;
+      let query = url.parse(request.url, true).query;
       expect(query['X-Client-Name']).to.be.equal('js-stellar-sdk');
       expect(query['X-Client-Version']).to.match(/^[0-9]+\.[0-9]+\.[0-9]+$/);
       response.end();
-      server.close(() => done());
-    }
+      server.close(() => {
+        closeStream();
+        done();
+      });
+    };
 
     server = http.createServer(requestHandler);
     server.listen(port, (err) => {
       if (err) {
         done(err);
-        return
+        return;
       }
 
-      new StellarSdk.Server(`http://localhost:${port}`, {allowHttp: true}).operations().stream();
-    })
+      closeStream = new StellarSdk.Server(`http://localhost:${port}`, {
+        allowHttp: true
+      })
+        .operations()
+        .stream();
+    });
   });
 });
