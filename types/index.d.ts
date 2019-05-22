22bf2fbadc57e04598a4b6dd12d1396d2c24bbd4
Akuukis <akuukis@kalvis.lv>
2019-05-22 22:18:55 +0300

    refactor(ts): migrate federation_server

--- types/index.d.ts
+++ types/index.d.ts
@@ -542,37 +542,6 @@ export namespace Server {
   }
 }
 
-export class FederationServer {
-  static createForDomain(
-    domain: string,
-    options?: FederationServer.Options
-  ): Promise<FederationServer>;
-  static resolve(
-    value: string,
-    options?: FederationServer.Options
-  ): Promise<FederationServer.Record>;
-
-  constructor(
-    serverURL: string,
-    domain: string,
-    options?: FederationServer.Options
-  );
-  resolveAccountId(account: string): Promise<FederationServer.Record>;
-  resolveAddress(address: string): Promise<FederationServer.Record>;
-  resolveTransactionId(transactionId: string): Promise<FederationServer.Record>;
-}
-export namespace FederationServer {
-  interface Record {
-    account_id: string;
-    memo_type?: string;
-    memo?: string;
-  }
-
-  interface Options {
-    allowHttp: boolean;
-  }
-}
-
 export namespace StellarTomlResolver {
   interface StellarTomlResolveOptions {
     allowHttp?: boolean;
