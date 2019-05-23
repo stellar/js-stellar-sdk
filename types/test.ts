e7db10f5fee7eda3d77a9a56538710e84b8a87e2
Akuukis <akuukis@kalvis.lv>
2019-05-23 16:24:18 +0300

    refactor(ts): remove d.ts

--- types/test.ts
+++
@@ -1,6 +0,0 @@
-import * as StellarSdk from 'stellar-sdk';
-
-StellarSdk.StellarTomlResolver.resolve('example.com', {
-  allowHttp: true,
-  timeout: 100
-}).then((toml: any) => toml.FEDERATION_SERVER);
