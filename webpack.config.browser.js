0899f561152fff5901c313f6cc4e877bdc25d62d
Morley Zhi <morley@stellar.org>
2019-06-11 14:04:28 -0400

    move ts-loader to devDependencies; try to transpile crc

--- webpack.config.browser.js
+++ webpack.config.browser.js
@@ -29,7 +29,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /node_modules\/(?!(crc)\/).*/,
         use: [
           {
             loader: "babel-loader",
