13d40b11b0775f74f6df0d76cd2b35be4e463af4
Morley Zhi <morley@stellar.org>
2019-06-12 10:49:28 -0400

    Support flags in version numbers (#325)

--- test/integration/client_headers_test.js
+++ test/integration/client_headers_test.js
@@ -1,20 +1,20 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 const port = 3100;
 
-describe('integration tests: client headers', function(done) {
-  if (typeof window !== 'undefined') {
+describe("integration tests: client headers", function(done) {
+  if (typeof window !== "undefined") {
     done();
     return;
   }
 
-  it('sends client via headers', function(done) {
+  it("sends client via headers", function(done) {
     let server;
 
     const requestHandler = (request, response) => {
-      expect(request.headers['x-client-name']).to.be.equal('js-stellar-sdk');
-      expect(request.headers['x-client-version']).to.match(
-        /^[0-9]+\.[0-9]+\.[0-9]+$/
+      expect(request.headers["x-client-name"]).to.be.equal("js-stellar-sdk");
+      expect(request.headers["x-client-version"]).to.match(
+        /^[0-9]+\.[0-9]+\.[0-9]+(-[a-z]+)?$/,
       );
       response.end();
       server.close(() => done());
@@ -33,15 +33,17 @@ describe('integration tests: client headers', function(done) {
     });
   });
 
-  it('sends client data via get params when streaming', function(done) {
+  it("sends client data via get params when streaming", function(done) {
     let server;
     let closeStream;
 
     const requestHandler = (request, response) => {
       // eslint-disable-next-line node/no-deprecated-api
       let query = url.parse(request.url, true).query;
-      expect(query['X-Client-Name']).to.be.equal('js-stellar-sdk');
-      expect(query['X-Client-Version']).to.match(/^[0-9]+\.[0-9]+\.[0-9]+$/);
+      expect(query["X-Client-Name"]).to.be.equal("js-stellar-sdk");
+      expect(query["X-Client-Version"]).to.match(
+        /^[0-9]+\.[0-9]+\.[0-9]+(-[a-z]+)?$/,
+      );
       response.end();
       server.close(() => {
         closeStream();
@@ -57,13 +59,13 @@ describe('integration tests: client headers', function(done) {
       }
 
       closeStream = new StellarSdk.Server(`http://localhost:${port}`, {
-        allowHttp: true
+        allowHttp: true,
       })
         .operations()
         .stream({
           onerror: (err) => {
             done(err);
-          }
+          },
         });
     });
   });
