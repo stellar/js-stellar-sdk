9a8dc5cb7b6d41938bea61843fa20c8514ab62df
Adolfo Builes <builes.adolfo@gmail.com>
2019-07-23 14:34:29 -0500

    Add SEP0010 transaction challenge builder. (#375)
    *  Implement SEP0010 transaction challenge builder.
    
    * Improve docs.
    
    * Use BASE_FEE instead of fixed number.
    
    * Add example in jsdoc.
    
    * Add randombytes.
    
    * Use randomBytes instead of crypto.randomBytes.

---
+++ src/utils.ts
@@ -0,0 +1,66 @@
+import randomBytes from "randombytes";
+import {
+  Account,
+  BASE_FEE,
+  Keypair,
+  Operation,
+  TransactionBuilder,
+} from "stellar-base";
+
+/**
+ * @namespace Utils
+ */
+export namespace Utils {
+  /**
+   * Returns a valid [SEP0010](https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0010.md)
+   * challenge transaction which you can use for Stellar Web Authentication.
+   *
+   * @see [SEP0010: Stellar Web Authentication](https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0010.md)
+   * @function
+   * @memberof Utils
+   * @param {Keypair} serverKeypair Keypair for server's signing account.
+   * @param {string} clientAccountID The stellar account that the wallet wishes to authenticate with the server.
+   * @param {string} anchorName Anchor's name to be used in the manage_data key.
+   * @param {number} [timeout=300] Challenge duration (default to 5 minutes).
+   * @example
+   * import { Utils, Keypair, Network }  from 'stellar-sdk'
+   *
+   * Network.useTestNetwork();
+   *
+   * let serverKeyPair = Keypair.fromSecret("server-secret")
+   * let challenge = Utils.buildChallengeTx(serverKeyPair, "client-stellar-account-id", "SDF", 300)
+   * @returns {string} A base64 encoded string of the raw TransactionEnvelope xdr struct for the transaction.
+   */
+  export function buildChallengeTx(
+    serverKeypair: Keypair,
+    clientAccountID: string,
+    anchorName: string,
+    timeout: number = 300,
+  ): string {
+    const account = new Account(serverKeypair.publicKey(), "-1");
+    const now = Math.floor(Date.now() / 1000);
+
+    const transaction = new TransactionBuilder(account, {
+      fee: BASE_FEE,
+      timebounds: {
+        minTime: now,
+        maxTime: now + timeout,
+      },
+    })
+      .addOperation(
+        Operation.manageData({
+          name: `${anchorName} auth`,
+          value: randomBytes(64),
+          source: clientAccountID,
+        }),
+      )
+      .build();
+
+    transaction.sign(serverKeypair);
+
+    return transaction
+      .toEnvelope()
+      .toXDR("base64")
+      .toString();
+  }
+}
