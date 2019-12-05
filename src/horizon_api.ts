195d3650f5736c7af9f014638bde07030da8f9ae
Adolfo Builes <builes.adolfo@gmail.com>
2019-12-05 15:31:51 -0500

    Removea all *_accepted_fee from FeeStatsResponse.

--- src/horizon_api.ts
+++ src/horizon_api.ts
@@ -332,19 +332,6 @@ export namespace Horizon {
     last_ledger: string;
     last_ledger_base_fee: string;
     ledger_capacity_usage: string;
-    min_accepted_fee: string;
-    mode_accepted_fee: string;
-    p10_accepted_fee: string;
-    p20_accepted_fee: string;
-    p30_accepted_fee: string;
-    p40_accepted_fee: string;
-    p50_accepted_fee: string;
-    p60_accepted_fee: string;
-    p70_accepted_fee: string;
-    p80_accepted_fee: string;
-    p90_accepted_fee: string;
-    p95_accepted_fee: string;
-    p99_accepted_fee: string;
     fee_charged: FeeDistribution;
     max_fee: FeeDistribution;
   }
