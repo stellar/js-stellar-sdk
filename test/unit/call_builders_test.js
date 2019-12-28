5ab651ead49a3fc4632afefc240b20b1a99e3a41
Clément Hallet <clement@challet.eu>
2019-12-28 15:57:59 +0100

    add a trailing '/' to domain

--- test/unit/call_builders_test.js
+++ test/unit/call_builders_test.js
@@ -4,13 +4,13 @@ const CallBuilder = require('../../lib/call_builder').CallBuilder;
 describe('CallBuilder functions', function() {
   
   it('doesn\'t mutate the constructor passed url argument (it clones it instead)', function() {
-    let arg = URI('https://onedomain');
+    let arg = URI('https://onedom.ain/');
     const builder = new CallBuilder(arg);
     builder.url.segment('one_segment');
     builder.checkFilter();
     
-    expect(arg.toString()).not.to.be.equal('https://onedomain');
-    expect(builder.url.toString()).to.be.equal('https://onedomain/one_segment');
+    expect(arg.toString()).not.to.be.equal('https://onedom.ain/one_segment'); // https://onedom.ain/
+    expect(builder.url.toString()).to.be.equal('https://onedom.ain/one_segment');
   });
   
 });
