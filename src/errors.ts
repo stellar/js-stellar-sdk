355c750de8aa6726349a7832ed1347f8c2210d6f
Adolfo Builes <builes.adolfo@gmail.com>
2020-03-25 12:41:27 -0500

    Add more info to AccountRequiresMemoError.

--- src/errors.ts
+++ src/errors.ts
@@ -72,15 +72,33 @@ export class InvalidSep10ChallengeError extends Error {
  * operation to an account which requires a memo. See
  * [SEP0029](https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0029.md)
  * for more information.
- * @class AccountRequiresMemoError
- * @hideconstructor
+ *
+ * This error contains two attributes to help you identify the account requiring
+ * the memo and the operation where the account is the destination
+ *
+ * ```
+ * console.log('The following account requires a memo ', err.accountId)
+ * console.log('The account is used in operation: ', err.operationIndex)
+ * ```
  *
  */
 export class AccountRequiresMemoError extends Error {
   public __proto__: AccountRequiresMemoError;
+  /**
+   * accountId account which requires a memo.
+   */
   public accountId: string;
+  /**
+   * operationIndex operation where accountId is the destination.
+   */
   public operationIndex: number;
 
+  /**
+   * Create an AccountRequiresMemoError
+   * @param {message} message - error message
+   * @param {string} accountId - The account which requires a memo.
+   * @param {number} operationIndex - The index of the operation where `accountId` is the destination.
+   */
   constructor(message: string, accountId: string, operationIndex: number) {
     const trueProto = new.target.prototype;
     super(message);
