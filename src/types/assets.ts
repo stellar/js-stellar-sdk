c2274dd77abebadb291f9a5505a18cc423d366b8
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-04-06 11:22:38 -0500

    Add fields to AssetRecord definition (#753)

--- src/types/assets.ts
+++ src/types/assets.ts
@@ -1,5 +1,6 @@
 import { AssetType } from "stellar-base";
 import { Horizon } from "./../horizon_api";
+
 export interface AssetRecord extends Horizon.BaseResponse {
   asset_type: AssetType.credit4 | AssetType.credit12;
   asset_code: string;
@@ -7,9 +8,11 @@ export interface AssetRecord extends Horizon.BaseResponse {
   paging_token: string;
   accounts: Horizon.AssetAccounts;
   num_claimable_balances: number;
+  num_liquidity_pools: number;
   balances: Horizon.AssetBalances;
   claimable_balances_amount: string;
   amount: string;
   num_accounts: number;
+  liquidity_pools_amount: string;
   flags: Horizon.Flags;
 }
