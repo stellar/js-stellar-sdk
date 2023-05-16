5613dca8d9bab26fe3ebe2ff66abde89c4d4d9e1
George <Shaptic@users.noreply.github.com>
2023-05-16 12:26:10 -0700

    Move dependencies into dev section, limit polyfills (#825)

--- config/webpack.config.browser.js
+++ config/webpack.config.browser.js
@@ -62,7 +62,9 @@ const config = {
     }),
     // Ignore native modules (sodium-native)
     new webpack.IgnorePlugin({ resourceRegExp: /sodium-native/ }),
-    new NodePolyfillPlugin(),
+    new NodePolyfillPlugin({
+      includeAliases: ["http", "https"] // others aren't needed
+    }),
     new webpack.ProvidePlugin({
       Buffer: ['buffer', 'Buffer']
     })
