1ebc4daa0893d6a520188eacfcac702bfc9b482e
George Kudrayvtsev <george@stellar.org>
2023-08-15 15:42:46 -0700

    Fixup browser tests

--- src/browser.ts
+++ src/browser.ts
@@ -2,6 +2,7 @@
 
 export * from './index';
 export * as StellarBase from 'stellar-base';
+export * as SorobanClient from './soroban';
 
 import axios from 'axios'; // idk why axios is weird
 export { axios };
--- test/test-browser.js
+++ test/test-browser.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-undef */
 chai.use(require('chai-as-promised'));
+window.SorobanClient = StellarSdk.SorobanClient;
 window.axios = StellarSdk.axios;
 window.AxiosClient = StellarSdk.AxiosClient;
+
+global.serverUrl = 'https://horizon-live.stellar.org:1337/api/v1/jsonrpc';
