0d7359cb0b5b0c054e32aa2bd3918682c4cc0780
George Kudrayvtsev <george@stellar.org>
2023-08-15 14:44:28 -0700

    Move files around

--- test/integration/client_headers_test.js
+++ test/integration/client_headers_test.js
@@ -2,8 +2,6 @@ const http = require("http");
 const url = require("url");
 const port = 3100;
 
-const versionPattern = /^[0-9]+\.[0-9]+\.[0-9]+(-[a-z]+(\.[0-9])?)?$/;
-
 describe("integration tests: client headers", function (done) {
   if (typeof window !== "undefined") {
     done();
@@ -14,8 +12,10 @@ describe("integration tests: client headers", function (done) {
     let server;
 
     const requestHandler = (request, response) => {
-      expect(request.headers["x-client-name"]).to.be.equal("js-stellar-sdk");
-      expect(request.headers["x-client-version"]).to.match(versionPattern);
+      expect(request.headers["x-client-name"]).to.be.equal("js-soroban-client");
+      expect(request.headers["x-client-version"]).to.match(
+        /^[0-9]+\.[0-9]+\.[0-9]+(-[a-z]+(\.[0-9])?)?$/
+      );
       response.end();
       server.close(() => done());
     };
@@ -27,44 +27,9 @@ describe("integration tests: client headers", function (done) {
         return;
       }
 
-      new StellarSdk.Server(`http://localhost:${port}`, { allowHttp: true })
-        .operations()
-        .call();
-    });
-  });
-
-  it("sends client data via get params when streaming", function (done) {
-    let server;
-    let closeStream;
-
-    const requestHandler = (request, response) => {
-      // eslint-disable-next-line node/no-deprecated-api
-      let query = url.parse(request.url, true).query;
-      expect(query["X-Client-Name"]).to.be.equal("js-stellar-sdk");
-      expect(query["X-Client-Version"]).to.match(versionPattern);
-      response.end();
-      server.close(() => {
-        closeStream();
-        done();
-      });
-    };
-
-    server = http.createServer(requestHandler);
-    server.listen(port, (err) => {
-      if (err) {
-        done(err);
-        return;
-      }
-
-      closeStream = new StellarSdk.Server(`http://localhost:${port}`, {
+      new SorobanClient.Server(`http://localhost:${port}`, {
         allowHttp: true,
-      })
-        .operations()
-        .stream({
-          onerror: (err) => {
-            done(err);
-          },
-        });
+      }).getHealth();
     });
   });
 });
