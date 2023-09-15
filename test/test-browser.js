aa9f9d68734a0a560d83afe567b8283376fafdfe
George Kudrayvtsev <george@stellar.org>
2023-09-14 17:09:52 -0700

    Fixup browser tests, too, LFGGGG

--- test/test-browser.js
+++ test/test-browser.js
@@ -3,3 +3,4 @@ chai.use(require("chai-as-promised"));
 window.axios = StellarSdk.axios;
 window.HorizonAxiosClient = StellarSdk.HorizonAxiosClient;
 window.SorobanAxiosClient = StellarSdk.SorobanAxiosClient;
+window.serverUrl = 'https://horizon-live.stellar.org:1337/api/v1/jsonrpc';
