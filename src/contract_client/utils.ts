3f134730d015602bf8f964a3c7334c8fb372bff7
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2024-05-03 15:27:30 -0400

    feat(contract-client): match CLI's default timeout
    Even 60 seconds, as it was updated to in
    2bcbc6a4acfb67078794059802a479b0b0473c0f, might be too short if someone
    is cautiously reviewing the transaction in Freighter, or if they need ot
    unlock their Ledger and walk through its process.
    
    Five minutes is the same as the CLI. Maybe it's a reasonable standard to
    adopt?
    
    This commit also updates utils.ts to use our eslint settings.

--- src/contract_client/utils.ts
+++ src/contract_client/utils.ts
@@ -1,7 +1,7 @@
 /**
  * The default timeout for waiting for a transaction to be included in a block.
  */
-export const DEFAULT_TIMEOUT = 60;
+export const DEFAULT_TIMEOUT = 5 * 60;
 
 /**
  * Keep calling a `fn` for `timeoutInSeconds` seconds, if `keepWaitingIf` is true.
@@ -12,7 +12,7 @@ export async function withExponentialBackoff<T>(
   keepWaitingIf: (result: T) => boolean,
   timeoutInSeconds: number,
   exponentialFactor = 1.5,
-  verbose = false
+  verbose = false,
 ): Promise<T[]> {
   const attempts: T[] = [];
 
@@ -34,7 +34,7 @@ export async function withExponentialBackoff<T>(
       console.info(
         `Waiting ${waitTime}ms before trying again (bringing the total wait time to ${totalWaitTime}ms so far, of total ${
           timeoutInSeconds * 1000
-        }ms)`
+        }ms)`,
       );
     }
     await new Promise((res) => setTimeout(res, waitTime));
@@ -56,8 +56,8 @@ export async function withExponentialBackoff<T>(
         } prev attempts. Most recent: ${JSON.stringify(
           attempts[attempts.length - 1],
           null,
-          2
-        )}`
+          2,
+        )}`,
       );
     }
   }
@@ -77,6 +77,8 @@ export const contractErrorPattern = /Error\(Contract, #(\d+)\)/;
 /**
  * A TypeScript type guard that checks if an object has a `toString` method.
  */
-export function implementsToString(obj: unknown): obj is { toString(): string } {
+export function implementsToString(
+  obj: unknown,
+): obj is { toString(): string } {
   return typeof obj === "object" && obj !== null && "toString" in obj;
 }
