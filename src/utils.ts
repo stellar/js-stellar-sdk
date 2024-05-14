9d663a1c3f9f21f695d5ed316cc23cb215d72114
Elliot Voris <elliot@stellar.org>
2024-05-14 10:56:59 -0500

    create a util namespace and add validateTimebounds to it

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,15 @@
 import { Transaction } from "@stellar/stellar-base";
 
+/** @namespace Utils */
 export class Utils {
   /**
    * Verifies if the current date is within the transaction's timebonds
    *
    * @static
    * @function
+   * @memberof Utils
    * @param {Transaction} transaction the transaction whose timebonds will be validated.
+   * @param {number} [gracePeriod=0] an additional window of time that should be considered valid on either end of the transaction's time range.
    * @returns {boolean} returns true if the current time is within the transaction's [minTime, maxTime] range.
    */
   static validateTimebounds(
