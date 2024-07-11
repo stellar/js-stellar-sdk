b5e8faa35384ee23df43b5d8e62c99c9aa0281ec
Blaine Heffron <baheffron@gmail.com>
2024-07-11 10:45:07 -0400

    rerun contract init if example repo updated

--- test/e2e/initialize.sh
+++ test/e2e/initialize.sh
@@ -36,16 +36,18 @@ echo "  RPC:        $SOROBAN_RPC_URL"
 echo "  Passphrase: \"$SOROBAN_NETWORK_PASSPHRASE\""
 echo "  Status:     $NETWORK_STATUS"
 
-if [[ "$NETWORK_STATUS" != "healthy" ]]; then
-  echo "Network is not healthy (not running?), exiting"
-  exit 1
-fi
+#if [[ "$NETWORK_STATUS" != "healthy" ]]; then
+#  echo "Network is not healthy (not running?), exiting"
+#  exit 1
+#fi
 
-$soroban keys generate $SOROBAN_ACCOUNT
+#$soroban keys generate $SOROBAN_ACCOUNT
 
 # retrieve the contracts using soroban contract init then build them if they dont already exist
 # Define directory and WASM file paths
 target_dir="$dirname/test-contracts/target/wasm32-unknown-unknown/release"
+contracts_dir="$dirname/test-contracts"
+repo_url="https://github.com/stellar/soroban-examples.git"
 wasm_files=(
     "soroban_custom_types_contract.wasm"
     "soroban_atomic_swap_contract.wasm"
@@ -54,7 +56,22 @@ wasm_files=(
     "hello_world.wasm"
 )
 
-# Check if all WASM files exist
+get_remote_git_hash() {
+    git ls-remote "$repo_url" HEAD | cut -f1
+}
+
+# Get the current git hash
+current_hash=$(get_remote_git_hash)
+
+# Check if a stored hash exists
+hash_file="$contracts_dir/.last_build_hash"
+if [ -f "$hash_file" ]; then
+    stored_hash=$(cat "$hash_file")
+else
+    stored_hash=""
+fi
+
+# Check if all WASM files exist and if the git hash has changed
 all_exist=true
 for wasm_file in "${wasm_files[@]}"; do
     if [ ! -f "$target_dir/$wasm_file" ]; then
@@ -63,16 +80,17 @@ for wasm_file in "${wasm_files[@]}"; do
     fi
 done
 
-# If any WASM file is missing, initialize and build the contracts
-if [ "$all_exist" = false ]; then
-    echo "One or more WASM files are missing. Initializing and building contracts..."
-    
+# If any WASM file is missing or the git hash has changed, initialize and build the contracts
+if [ "$all_exist" = false ] || [ "$current_hash" != "$stored_hash" ]; then
+    echo "WASM files are missing or contracts have been updated. Initializing and building contracts..."
     # Initialize contracts
     $soroban contract init "$dirname/test-contracts" --with-example increment custom_types atomic_swap token
     
     # Change directory to test-contracts and build the contracts
     cd "$dirname/test-contracts" || { echo "Failed to change directory!"; exit 1; }
     $soroban contract build
+    # Save git hash to file
+    echo "$current_hash" > "$hash_file"
 else
-    echo "All WASM files are present."
+    echo "All WASM files are present and up to date."
 fi
