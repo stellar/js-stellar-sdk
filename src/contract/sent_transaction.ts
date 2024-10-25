594ae15ecf645083ddc33eafa5e73f7f1e01b354
George <george@stellar.org>
2024-10-24 19:32:27 -0700

    Incorporate it into SentTransaction

--- src/contract/sent_transaction.ts
+++ src/contract/sent_transaction.ts
@@ -3,7 +3,6 @@
 import type { MethodOptions } from "./types";
 import { Server } from "../rpc"
 import { Api } from "../rpc/api"
-import { withExponentialBackoff } from "./utils";
 import { DEFAULT_TIMEOUT } from "./types";
 import type { AssembledTransaction } from "./assembled_transaction";
 
@@ -94,22 +93,20 @@ export class SentTransaction<T> {
 
     const { hash } = this.sendTransactionResponse;
 
-    const timeoutInSeconds =
-      this.assembled.options.timeoutInSeconds ?? DEFAULT_TIMEOUT;
-    this.getTransactionResponseAll = await withExponentialBackoff(
-      () => this.server.getTransaction(hash),
-      (resp) => resp.status === Api.GetTransactionStatus.NOT_FOUND,
-      timeoutInSeconds,
-    );
+    // The transaction will last for this many seconds:
+    const timeoutSec = this.assembled.options.timeoutInSeconds ?? DEFAULT_TIMEOUT;
+    this.getTransactionResponse = await this.server.pollTransaction(hash, {
+      // 4s baseline + 0.5s per iteration linear backoff
+      sleepStrategy: (iter: number) => 4000 + (500 * iter),
+      attempts: Math.ceil(timeoutSec / 4),
+    });
 
-    this.getTransactionResponse =
-      this.getTransactionResponseAll[this.getTransactionResponseAll.length - 1];
     if (
       this.getTransactionResponse.status ===
       Api.GetTransactionStatus.NOT_FOUND
     ) {
       throw new SentTransaction.Errors.TransactionStillPending(
-        `Waited ${timeoutInSeconds} seconds for transaction to complete, but it did not. ` +
+        `Waited ${timeoutSec} seconds for transaction to complete, but it did not. ` +
         `Returning anyway. Check the transaction status manually. ` +
         `Sent transaction: ${JSON.stringify(
           this.sendTransactionResponse,
