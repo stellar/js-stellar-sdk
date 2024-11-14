53520e2cf6f0cbcfe51e7776de512fe16fe37d28
George <george@stellar.org>
2024-11-14 13:58:45 -0800

    Fetch metadata from RPC instead of friendbot

--- src/friendbot/index.ts
+++ src/friendbot/index.ts
@@ -1,6 +1,7 @@
 export namespace Api {
   // Just the fields we are interested in
   export interface Response {
+    hash: string;
     result_meta_xdr: string;
   }
 }
--- src/rpc/server.ts
+++ src/rpc/server.ts
@@ -977,11 +977,14 @@ export class RpcServer {
         `${friendbotUrl}?addr=${encodeURIComponent(account)}`
       );
 
-      const meta = xdr.TransactionMeta.fromXDR(
-        response.data.result_meta_xdr,
-        'base64'
+      const txMeta = await this.getTransaction(response.data.hash)
+      if (txMeta.status !== Api.GetTransactionStatus.SUCCESS) {
+        throw new Error(`Funding account ${address} failed`);
+      }
+
+      const sequence = findCreatedAccountSequenceInTransactionMeta(
+        txMeta.resultMetaXdr
       );
-      const sequence = findCreatedAccountSequenceInTransactionMeta(meta);
       return new Account(account, sequence);
     } catch (error: any) {
       if (error.response?.status === 400) {
