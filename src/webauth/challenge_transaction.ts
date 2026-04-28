5151c62bd6e8e0c9c0e711bd29fa96e3b3eae474
Ryan Yang <ryan.yang@stellar.org>
2026-04-28 11:02:43 -0700

    fix removed import

--- src/webauth/challenge_transaction.ts
+++ src/webauth/challenge_transaction.ts
@@ -16,6 +16,7 @@ import { InvalidChallengeError } from "./errors.js";
 import { gatherTxSigners, verifyTxSignedBy } from "./utils.js";
 import { Utils } from "../utils.js";
 import { ServerApi } from "../horizon/server_api.js";
+import randomBytes from "randombytes";
 
 /**
  * Returns a valid {@link https://stellar.org/protocol/sep-10 | SEP-10}
