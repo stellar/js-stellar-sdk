5e0d7dd903625b8906ab66500d2176d0290c3e02
Ryan Yang <ryan.yang@stellar.org>
2026-07-24 10:14:27 -0700

    revert(webauthn): remove passkey signature ScVal builder and secp256r1 normalizer

--- src/base/index.ts
+++ src/base/index.ts
@@ -95,10 +95,5 @@ export type {
   AuthEntryInfo,
   AuthEntryReadiness,
 } from "./auth.js";
-export {
-  buildWebAuthnSignatureScVal,
-  normalizeSecp256r1Signature,
-} from "./webauthn.js";
-export type { WebAuthnSignatureParts } from "./webauthn.js";
 export * from "./invocation.js";
 export * from "./numbers/index.js";
